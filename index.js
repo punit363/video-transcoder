857ccfeb098a61b4fdaf6780397b27470ac3b487
Punit <3033.punit@gmail.com>
2025-07-24 12:06:20 +0530

    feat: setup multer to handle file upload

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, filename, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, filename, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +31,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Origin,X-Requested-With,Content-Type,Accept");
   next();
+});
+
+app.post("/upload", upload.single("file"), (req,res,_next) => {
+  console.log("file uploaded");
 });
 
 app.listen(8000, () => {
