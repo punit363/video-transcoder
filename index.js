7a62447f6d0558a03b5d9501f13ab9a5de369b82
Punit <3033.punit@gmail.com>
2025-07-24 12:00:59 +0530

    feat: setup cors

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from "express";
 import cors from "cors";
-
+import multer from "multer";
+import path from "path";
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use("uploads",express.static("uploads"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("uploads", express.static("uploads"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
-app.listen(8000,()=>{
-    console.log("App listening on port 8000...")
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Origin,X-Requested-With,Content-Type,Accept");
+  next();
+});
+
+app.listen(8000, () => {
+  console.log("App listening on port 8000...");
+});
